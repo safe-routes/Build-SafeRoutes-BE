af4c3fcc09ce3c3761e9275a4470cb705c60c3f9
Brian Illes <brianrilles@gmail.com>
2019-04-14 13:03:49 -0700

    Removes password from return

--- users/users-model.js
+++ users/users-model.js
@@ -9,20 +9,19 @@ module.exports = {
 
 function getUserById(id) {
   return db('users')
-    .select()
-    .where('id', id)
+    .select('id', 'email', 'name', 'username', 'created_at')
+    .where({ id })
     .first();
 }
 
-// NOTE: this fuction is written in this convoluted manner because of an error that was occuring in the deploy enviroment.
+// NOTE: this function is written in this convoluted manner because of an error that was occuring in the deploy enviroment.
 function addUser(user) {
   db('users')
     .insert(user)
     .then(ids => {
       const firstId = ids[0];
-
       db('users')
-        .select()
+        .select('id', 'email', 'name', 'username', 'created_at')
         .where('id', firstId)
         .first()
         .then(user => {
