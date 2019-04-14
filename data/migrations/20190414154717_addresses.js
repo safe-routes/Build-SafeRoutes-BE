05d0489e39ce5b926a0f14cdb1e03f7fc7d4d39d
Brian Illes <brianrilles@gmail.com>
2019-04-14 15:59:45 -0700

    Runs Addresses migration

---
+++ data/migrations/20190414154717_addresses.js
@@ -0,0 +1,21 @@
+exports.up = function(knex) {
+  return knex.schema.createTable('addresses', addresses => {
+    addresses.increments();
+    addresses
+      .integer('user_id')
+      .unsigned()
+      .notNullable()
+      .references('users.id')
+      .onDelete('CASCADE')
+      .onUpdate('CASCADE');
+    addresses
+      .string('address', 256)
+      .notNullable()
+      .unique();
+    addresses.timestamp('created_at').defaultTo(knex.fn.now());
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists('addresses');
+};
