babf83fb83e8759c072f564c3c24152737e8b0cf
Brian Illes <brianrilles@gmail.com>
2019-04-15 11:31:39 -0700

    Deploy fix

--- users/users-model.js
+++ users/users-model.js
@@ -15,13 +15,21 @@ function getUserById(id) {
 }
 
 // NOTE: this function is written in this manner because of an error that was occuring in the deploy enviroment.
-async function addUser(user) {
-  const [id] = await db('users').insert(user);
-  // { email: user.email, name: user.name, username: user.username };
-  return db('users')
-    .select('id', 'email', 'name', 'username', 'created_at')
-    .where({ id })
-    .first();
+function addUser(user) {
+  db('users')
+    .insert(user)
+    .then(ids => {
+      const firstId = ids[0];
+      db('users')
+        .select('id', 'email', 'name', 'username', 'created_at')
+        .where('id', firstId)
+        .first()
+        .then(user => {
+          return user;
+        });
+      return user;
+    });
+  return { email: user.email, name: user.name, username: user.username };
 }
 
 function getUserByUsername(username) {
