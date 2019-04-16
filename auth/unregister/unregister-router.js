946c9a066a94c8f04b65bbad182e0d3b7471f45c
Brian Illes <brianrilles@gmail.com>
2019-04-16 15:14:10 -0700

    changes unregister logic

--- auth/unregister/unregister-router.js
+++ auth/unregister/unregister-router.js
@@ -7,23 +7,17 @@ router.delete('/', async (req, res) => {
   let user = req.body;
   const { username, password } = user;
 
-  if (!username || !password) {
-    res.status(422).json({
-      message: 'Must provide username and password.'
-    });
-  } else {
-    try {
-      const user = await Users.getUserByUsername(username);
-      if (user && bcrypt.compareSync(password, user.password)) {
-        const deletedUserCount = await Users.removeUser(username);
-        if (deletedUserCount) {
-          res.status(204).end();
-        } else res.status(404).json({ message: 'User not found.' });
-      }
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ message: 'User could not be unregistered' });
+  try {
+    const user = await Users.getUserByUsername(username);
+    if (user && bcrypt.compareSync(password, user.password)) {
+      const deletedUserCount = await Users.removeUser(username);
+      if (deletedUserCount) {
+        res.status(204).end();
+      } else res.status(404).json({ message: 'User not found.' });
     }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'User could not be unregistered' });
   }
 });
 
