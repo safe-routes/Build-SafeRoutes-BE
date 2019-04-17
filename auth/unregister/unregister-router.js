f84f11a526d5c76f92892d98b4c2239cc1c193db
Brian Illes <brianrilles@gmail.com>
2019-04-17 09:31:04 -0700

    Changes /api/user/:id DELETE logic

--- auth/unregister/unregister-router.js
+++ auth/unregister/unregister-router.js
@@ -1,36 +1,19 @@
 const router = require('express').Router();
-const bcrypt = require('bcryptjs');
 
 const Users = require('../../users/users-model.js');
 
-// router.delete('/:id', async (req, res) => {
-//   let id = req.params.id;
-//   try {
-//     await Users.removeUser(id);
-//     res.status(204).end();
-//   } catch (error) {
-//     res.status(500).json({ message: 'User could not be unregistered' });
-//   }
-// });
-
 router.delete('/:id', async (req, res) => {
-  const id = req.params.id;
-  const { username, password } = req.body.data;
-
+  let id = req.params.id;
   try {
-    const user = await Users.getUserByUsername(username);
-    if (user && bcrypt.compareSync(password, user.password)) {
-      try {
-        await Users.removeUser(id);
-        res.status(204).end();
-      } catch (error) {
-        res.status(404).json({ message: 'User not found.' });
-      }
+    const user = await Users.getUserById(id);
+    if (user) {
+      await Users.removeUser(id);
+      res.status(204).end();
     } else {
-      res.status(500).json({ message: 'Invalid credentials.' });
+      res.status(404).json({ message: 'User not found.' });
     }
   } catch (error) {
-    res.status(500).json({ message: 'User could not be deleted.' });
+    res.status(500).json({ message: 'User could not be unregistered' });
   }
 });
 
