7b3cf6146bc8e0a8cf2571eb7aa259469d4fe9ae
Brian Illes <brianrilles@gmail.com>
2019-04-18 12:29:51 -0700

    changes lats and longs to numeric

--- data/migrations/20190418094937_saferoutes.js
+++ data/migrations/20190418094937_saferoutes.js
@@ -5,8 +5,8 @@ exports.up = function(knex) {
       accidents.string('TWAY_ID', 128);
       accidents.string('TWAY_ID2', 128);
       accidents.string('COUNTY', 128);
-      accidents.float('LATITUDE', 12, 12);
-      accidents.float('LONGITUD', 12, 12);
+      accidents.numeric('LATITUDE');
+      accidents.numeric('LONGITUD');
       accidents.integer('MONTH', 128);
       accidents.integer('DAY', 128);
       accidents.integer('YEAR', 128);
