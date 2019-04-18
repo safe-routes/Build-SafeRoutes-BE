affd6537c448f8de655c53936dd9f8e2cdf3a599
Brian Illes <brianrilles@gmail.com>
2019-04-18 12:22:59 -0700

    changes float precision

--- data/migrations/20190418094937_saferoutes.js
+++ data/migrations/20190418094937_saferoutes.js
@@ -5,8 +5,8 @@ exports.up = function(knex) {
       accidents.string('TWAY_ID', 128);
       accidents.string('TWAY_ID2', 128);
       accidents.string('COUNTY', 128);
-      accidents.float('LATITUDE', 10, 7);
-      accidents.float('LONGITUD', 10, 7);
+      accidents.float('LATITUDE', 12, 12);
+      accidents.float('LONGITUD', 12, 12);
       accidents.integer('MONTH', 128);
       accidents.integer('DAY', 128);
       accidents.integer('YEAR', 128);
